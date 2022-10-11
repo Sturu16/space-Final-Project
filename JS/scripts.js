5c3964cbdfc877793cd82d1d7a6e7b94f1d5457e
Sturu16 <maristurua16@gmail.com>
2022-10-11 17:36:54 +0400

    final project

---
+++ JS/scripts.js
@@ -0,0 +1,38 @@
+function showmenu(){
+    document.getElementById("star").style.display="flex";
+}
+function hidemenu(){
+    document.getElementById("star").style.display="none";
+}
+
+
+
+var countDownDate = new Date("Dec 22, 2022 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+function gamochena(){
+  document.getElementById("msgbox").style.display = "block";
+}
+
+function gamortva(){
+  document.getElementById("msgbox").style.display = "none";
+}
